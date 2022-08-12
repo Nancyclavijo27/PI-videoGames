2de74d566ed61f0e291e380fb52259393cc7761e
Nancy Clavijo Varela <clavijovarela@gmail.com>
2022-08-12 18:30:32 -0500

    BD y models

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,8 +4,38 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    release_date: {
+      type: DataTypes.DATEONLY,
+    },
+    rating: {
+      type: DataTypes.FLOAT,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    platforms: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdDB: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
       allowNull: false,
     },
   });
